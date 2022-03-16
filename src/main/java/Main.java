fe7513efc78ec581fbe366e0ad8d67157fe62fd2
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-03-16 15:39:24 +0100

    add Logger to main main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    final static Logger logger = Logger.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 }
