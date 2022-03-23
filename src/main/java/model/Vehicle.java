b29a80e608c99e0bc033666b807546cd5236328e
Dominik Kroskowski <dominik.kroskowski@ergohestia.pl>
2022-03-23 13:34:10 +0100

    Add properties for vehicle class

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,9 +1,16 @@
 package model;
 
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 public class Vehicle {
+
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("type")
     private String type;
+
+    @JsonProperty("attributes")
     private VehicleAttributes attributes;
 }
