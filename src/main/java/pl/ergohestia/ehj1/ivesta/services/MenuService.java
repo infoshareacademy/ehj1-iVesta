5bbd7e7a1d472c4b02a45b384e3b9f8db6190d27
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-03-23 15:31:52 +0100

    refactoring

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/MenuService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/MenuService.java
@@ -1,36 +1,57 @@
 package pl.ergohestia.ehj1.ivesta.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.ergohestia.ehj1.ivesta.services.CarService;
+import pl.ergohestia.ehj1.ivesta.services.DriverService;
+import pl.ergohestia.ehj1.ivesta.services.RouteService;
+
 import java.util.Scanner;
 
 public class MenuService {
+
+    private static final Logger log = LoggerFactory.getLogger(MenuService.class);
+    private static final Logger SYSOUT = LoggerFactory.getLogger("SYSOUT");
 
     CarService carService = new CarService();
     DriverService driverService = new DriverService();
     RouteService routeService = new RouteService();
 
+    public void printMainMenu() {
+        SYSOUT.info("Witaj w aplikacji iVesta!\n");
+        SYSOUT.info("Wybierz jedną z poniższych opcji:");
+        SYSOUT.info("1. Wyświetl kierowców.");
+        SYSOUT.info("2. Wyświetl samochody.");
+        SYSOUT.info("3. Zaplanuj trasę.");
+    }
+
     public int getMenuItem() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Pozycja z menu: ");
-        int item = scanner.nextInt();
+        int item = 0;
+        do {
+            SYSOUT.info("Wybierz numer z menu: ");
+            if(!scanner.hasNextInt()){
+                scanner.next();
+                continue;
+            }
+            item = scanner.nextInt();
+        } while (item < 1);
+
         return item;
     }
 
-    //TODO sprawdzenie poprawności podawanych wartości w scannerze przez użytkownika
-    public void printMainMenu() {
-        System.out.println("Witaj w aplikacji iVesta!\n");
-        System.out.println("Wybierz jedną z poniższych opcji:");
-        System.out.println("1. Wyświetl kierowców.");
-        System.out.println("2. Wyświetl samochody.");
-        System.out.println("3. Zaplanuj trasę.");
-    }
-
-    public void handleMainManu(int item) {
+    public void serviceMainMenu(int item) {
         switch (item) {
             case 1 -> carService.printElements();
             case 2 -> driverService.printElements();
             case 3 -> routeService.printElements();
-            default -> System.out.println("Wybrałeś zły numer z menu");
+            default -> handleMainMenu();
         }
+    }
+
+    public void handleMainMenu(){
+        int menuItem = getMenuItem();
+        serviceMainMenu(menuItem);
     }
 
 
