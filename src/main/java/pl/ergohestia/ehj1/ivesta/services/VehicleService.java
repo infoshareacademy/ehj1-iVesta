f2e5ac2746a4daf7dfb799b562af8500f51c3505
Dominik Kroskowski <dominik.kroskowski@ergohestia.pl>
2022-03-28 13:15:49 +0200

    Fix vehicleLoader

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
@@ -3,18 +3,22 @@ package pl.ergohestia.ehj1.ivesta.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.ergohestia.ehj1.ivesta.model.Vehicle;
+import pl.ergohestia.ehj1.ivesta.repository.VehiclesLoader;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleService implements Service{
+
+    VehiclesLoader vehiclesLoader = new VehiclesLoader(Path.of("src/main/resources/input.json"));
 
     private static final Logger SYSOUT = LoggerFactory.getLogger("SYSOUT");
 
     private final List<Vehicle> vehicleList;
 
     public VehicleService() {
-        this.vehicleList = new ArrayList<>();
+        this.vehicleList = vehiclesLoader.getListOfVehicles();
     }
 
     public List<Vehicle> getVehiclesList(){
