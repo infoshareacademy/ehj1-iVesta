5d04a8f34b6d1065a94ed66bf46b2ddff421c84f
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-03-29 08:36:59 +0200

    change Route model

--- src/main/java/pl/ergohestia/ehj1/ivesta/model/Route.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/model/Route.java
@@ -8,28 +8,18 @@ import lombok.Setter;
 
 public class Route {
 
-    private String startCity;
     private String startAddress;
-    private String destinationCity;
     private String destinationAddress;
-    private String driverName;
-    private String driverLastName;
-    private String assignedVehicle;
+    private Integer routeLength;
     private String cargoType;
     private Integer cargoWeight;
-    private Integer routeLength;
 
-    public Route(String startCity, String startAddress, String destinationCity, String destinationAddress, String driverName, String driverLastName, String assignedVehicle, String cargoType, Integer cargoWeight, Integer routeLength) {
-        this.startCity = startCity;
+    public Route(String startAddress, String destinationAddress, Integer routeLength, String cargoType, Integer cargoWeight) {
         this.startAddress = startAddress;
-        this.destinationCity = destinationCity;
         this.destinationAddress = destinationAddress;
-        this.driverName = driverName;
-        this.driverLastName = driverLastName;
-        this.assignedVehicle = assignedVehicle;
+        this.routeLength = routeLength;
         this.cargoType = cargoType;
         this.cargoWeight = cargoWeight;
-        this.routeLength = routeLength;
     }
 
     public Route() {
