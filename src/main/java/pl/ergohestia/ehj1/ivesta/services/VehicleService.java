99fded576959287ad42c29a296d780fd1659883c
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-03-29 11:21:19 +0200

    add getVehiclePath Metode

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
@@ -2,14 +2,24 @@ package pl.ergohestia.ehj1.ivesta.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.ergohestia.ehj1.ivesta.configs.DefaultVehiclePath;
 import pl.ergohestia.ehj1.ivesta.model.Vehicle;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class VehicleService implements Service{
+public class VehicleService implements Service {
 
     private static final Logger SYSOUT = LoggerFactory.getLogger("SYSOUT");
+    Scanner scanner = new Scanner(System.in);
+
+    //TODO Create menu for Vehicle Services
+
+    public Path getVehiclePath() {
+        return getPath();
+    }
 
     private final List<Vehicle> vehicleList;
 
@@ -17,11 +27,11 @@ public class VehicleService implements Service{
         this.vehicleList = new ArrayList<>();
     }
 
-    public List<Vehicle> getVehiclesList(){
+    public List<Vehicle> getVehiclesList() {
         return vehicleList;
     }
 
-    public void addVehicleToList(Vehicle vehicle){
+    public void addVehicleToList(Vehicle vehicle) {
         vehicleList.add(vehicle);
     }
 
@@ -34,5 +44,19 @@ public class VehicleService implements Service{
     //TODO implementacja metody
     @Override
     public void addElement() {
+    }
+    private Path getPath() {
+        SYSOUT.info("Czy pliki dotyczące pojazdu mają byc wczytane z domyślen ścieżki? \n T\\N");
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("t")) {
+            DefaultVehiclePath defaultPath = new DefaultVehiclePath();
+            return defaultPath.vehiclePath;
+        } else if (answer.equalsIgnoreCase("n")) {
+            SYSOUT.info("Proszę o podanie pełnej ścieżki z lokalizacją pliku: ");
+            answer = scanner.nextLine();
+            DefaultVehiclePath defaultPath = new DefaultVehiclePath(answer);
+            return defaultPath.vehiclePath;
+        }
+        return getVehiclePath();
     }
 }
