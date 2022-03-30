9f79b960db03079333830cd7711e34aad9dc9258
polomi1 <michal.polom@ergohestia.pl>
2022-03-30 13:31:34 +0200

    add test class

---
+++ src/test/java/pl/ergohestia/ehj1/ivesta/services/MenuServiceTest.java
@@ -0,0 +1,19 @@
+package pl.ergohestia.ehj1.ivesta.services;
+
+import org.junit.jupiter.api.Test;
+import pl.ergohestia.ehj1.ivesta.ui.MenuService;
+
+class MenuServiceTest {
+
+    private MenuService sut;
+
+   @Test
+   void shouldName() {
+      // given
+
+      // when
+
+      // then
+
+   }
+}
