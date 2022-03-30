4374c2fc5fbd955c6df263a35611d8976c3218b0
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-03-30 16:55:12 +0200

    Add dao to packages

---
+++ src/test/java/pl/ergohestia/ehj1/ivesta/entities/DriverTest.java
@@ -0,0 +1,10 @@
+package pl.ergohestia.ehj1.ivesta.entities;
+
+import pl.ergohestia.ehj1.ivesta.model.DriverDto;
+import pl.ergohestia.ehj1.ivesta.utils.HibernateUtils;
+
+import javax.persistence.EntityManager;
+
+class DriverTest {
+
+}
