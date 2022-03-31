e4a7b4375db387822545b73ee98902b249a796bf
polomi1 <michal.polom@ergohestia.pl>
2022-03-31 16:12:23 +0200

    add getter to Manu class

--- src/main/java/pl/ergohestia/ehj1/ivesta/model/Menu.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/model/Menu.java
@@ -1,16 +1,16 @@
 package pl.ergohestia.ehj1.ivesta.model;
 
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
 public class Menu {
 
-    public List<String> menuItems;
+    private List<String> menuItems;
 
     public Menu(String... menuItem) {
         this.menuItems = List.of(menuItem);
     }
 
+    public List<String> getMenuItems() {
+        return menuItems;
+    }
 }
