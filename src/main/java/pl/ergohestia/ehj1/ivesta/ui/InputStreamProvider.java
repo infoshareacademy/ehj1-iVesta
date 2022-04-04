5e3568deb8e2a468216001e7239e7d8358addd5e
polomi1 <michal.polom@ergohestia.pl>
2022-04-04 15:29:54 +0200

    InputStreamProvider as Singletion and add Scanner handler

--- src/main/java/pl/ergohestia/ehj1/ivesta/ui/InputStreamProvider.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/ui/InputStreamProvider.java
@@ -1,9 +1,33 @@
 package pl.ergohestia.ehj1.ivesta.ui;
 
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class InputStreamProvider {
-    public InputStream getInputStream(){
-        return System.in;
+
+    private static InputStream inputStream = System.in;
+    private static Scanner scanner = new Scanner(inputStream);
+    private static InputStreamProvider inputStreamProvider;
+    private InputStreamProvider() {
+    }
+
+    public static InputStreamProvider getInstance() {
+        if (inputStreamProvider == null) {
+            inputStreamProvider = new InputStreamProvider();
+        }
+        return inputStreamProvider;
+    }
+
+    public static void setInputStream(InputStream inputStream) {
+        InputStreamProvider.inputStream = inputStream;
+        scanner = new Scanner(inputStream);
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public Scanner getScanner(){
+        return scanner;
     }
 }
