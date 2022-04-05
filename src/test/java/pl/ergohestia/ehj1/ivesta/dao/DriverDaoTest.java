dcf04225e8264e0ccc9770ef6ac729e27868db91
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-05 13:41:41 +0200

    remove when part

---
+++ src/test/java/pl/ergohestia/ehj1/ivesta/dao/DriverDaoTest.java
@@ -0,0 +1,29 @@
+package pl.ergohestia.ehj1.ivesta.dao;
+
+import org.junit.jupiter.api.Test;
+import pl.ergohestia.ehj1.ivesta.entities.Driver;
+import pl.ergohestia.ehj1.ivesta.model.DriverDto;
+
+class DriverDaoTest {
+
+    private DriverDao sut;
+
+
+    @Test
+    void shouldSaveNewDriver() {
+        // given
+        String name = "TestName";
+        String lastName= "TestLastName";
+        String address = "TeestAddress";
+        String phoneNumber = "TestPhone";
+        String license = "TestLicense";
+        Integer numberOfCourses = 5;
+        Integer numberOfKilometres = 3000;
+
+
+        // when
+
+        // then
+
+    }
+}
