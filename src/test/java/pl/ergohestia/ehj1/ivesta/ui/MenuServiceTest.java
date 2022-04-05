4d1967f630939cdc01f7d3c940e0c977baef53c5
polomi1 <michal.polom@ergohestia.pl>
2022-04-05 09:05:30 +0200

    working test but to refactor

--- src/test/java/pl/ergohestia/ehj1/ivesta/ui/MenuServiceTest.java
+++ src/test/java/pl/ergohestia/ehj1/ivesta/ui/MenuServiceTest.java
@@ -1,16 +1,20 @@
 package pl.ergohestia.ehj1.ivesta.ui;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.slf4j.Logger;
 import pl.ergohestia.ehj1.ivesta.model.Menu;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
@@ -23,41 +27,43 @@ class MenuServiceTest {
     private Menu menu;
 
     @Mock
-    private Logger SYSOUT;
+    Logger SYSOUT;
+
+    @Mock
+    InputScannerProvider inputScannerProvider;
 
     @InjectMocks
-    private MenuService sut = new MenuService();
+    private MenuService sut;
 
     @Captor
     private ArgumentCaptor<String> captor;
 
-
+    // TODO refatctor and change name
     @Test
     void shouldName() {
 
-        List<String> testMenu = List.of("1 test", "2 test", "3 test");
+        String test1 = "1 test";
+        List<String> testMenu = List.of(test1, "2 test", "3 test");
         when(menu.getMenuItems()).thenReturn(testMenu);
 
-        //dziala
         InputStream testInputStream = prepareInputStream("test", 999999, -1, 1);
-        InputStreamProvider.getInstance();
-        InputStreamProvider.setInputStream(testInputStream);
+        Scanner testScanner = new Scanner(testInputStream);
+
+        when(inputScannerProvider.getScanner()).thenReturn(testScanner);
 
         sut.menu();
 
-//        verify(SYSOUT).info(captor.capture());
-//        String value = captor.getValue();
-//        System.out.println(value);
-////        List<String> allValues = captor.getAllValues();
-//
-//        assertThat(true);
+        verify(SYSOUT, atLeastOnce()).info(captor.capture());
+        String lastCapturedMessage = captor.getAllValues().get(captor.getAllValues().size()-1);
+
+        String testMessage = lastCapturedMessage.substring(lastCapturedMessage.length() - 6);
+        assertThat(testMessage).isEqualTo(test1);
     }
 
     private InputStream prepareInputStream(Object... inputs) {
         StringBuilder testInput = new StringBuilder();
         for (Object input : inputs) {
             testInput.append(input).append(System.lineSeparator());
-//            testInput.append(input).append("\n");
         }
         return new ByteArrayInputStream(testInput.toString().getBytes());
     }
