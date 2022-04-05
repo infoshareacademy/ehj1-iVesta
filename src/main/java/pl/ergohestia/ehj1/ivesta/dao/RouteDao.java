7cf4eeaf6aa5955ceded931819ed3b298ba43ffb
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-04-05 12:03:28 +0200

    add RouteDao class

---
+++ src/main/java/pl/ergohestia/ehj1/ivesta/dao/RouteDao.java
@@ -0,0 +1,58 @@
+package pl.ergohestia.ehj1.ivesta.dao;
+
+import pl.ergohestia.ehj1.ivesta.adapters.RouteAdapter;
+import pl.ergohestia.ehj1.ivesta.entities.Route;
+import pl.ergohestia.ehj1.ivesta.model.RouteDto;
+import pl.ergohestia.ehj1.ivesta.utils.HibernateUtils;
+
+import javax.persistence.EntityManager;
+import java.util.Collection;
+import java.util.List;
+
+public class RouteDao implements Dao<RouteDto> {
+
+    private final EntityManager em = HibernateUtils.getEntityManager();
+    private final RouteAdapter routeAdapter = new RouteAdapter();
+
+    @Override
+    public RouteDto find(Long id) {
+        return routeAdapter.convertToRouteDto(em.find(Route.class, id));
+    }
+
+    @Override
+    public Collection<RouteDto> findAll() {
+        em.getTransaction().begin();
+        List<RouteDto> routes = em.createNamedQuery("route.findAll", Route.class)
+                .getResultStream()
+                .map(routeAdapter::convertToRouteDto)
+                .toList();
+        em.getTransaction().commit();
+        return routes;
+    }
+
+    private void saveRoute(Route route){
+        em.getTransaction().begin();
+        em.persist(route);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public void save(RouteDto routeDto) {
+        saveRoute(routeAdapter.convertToRoute(routeDto));
+    }
+
+    @Override
+    public RouteDto update(RouteDto routeDto) {
+        em.getTransaction().begin();
+        Route route = em.merge(routeAdapter.convertToRoute(routeDto));
+        em.getTransaction().commit();
+        return routeAdapter.convertToRouteDto(route);
+    }
+
+    @Override
+    public void delete(RouteDto routeDto) {
+        em.getTransaction().begin();
+        em.remove(routeAdapter.convertToRoute(routeDto));
+        em.getTransaction().commit();
+    }
+}
