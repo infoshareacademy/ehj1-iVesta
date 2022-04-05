944cc2bf0fdf4e7db49e160510e3e83e71f85d50
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-04-05 11:21:19 +0200

    change type in findAll method

--- src/main/java/pl/ergohestia/ehj1/ivesta/dao/VehicleDao.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/dao/VehicleDao.java
@@ -21,10 +21,11 @@ public class VehicleDao implements Dao<VehicleDto> {
     }
 
     @Override
-    public Collection<Vehicle> findAll() {
+    public Collection<VehicleDto> findAll() {
         em.getTransaction().begin();
-        List<Vehicle> vehicles = em.createNamedQuery("vehicle.findAll", Vehicle.class)
+        List<VehicleDto> vehicles = em.createNamedQuery("vehicle.findAll", Vehicle.class)
                 .getResultStream()
+                .map(vehicleAdapter::convertToVehicleDto)
                 .toList();
         em.getTransaction().commit();
         return vehicles;
