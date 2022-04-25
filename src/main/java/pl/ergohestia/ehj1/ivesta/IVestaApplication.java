6f13edcd6da36397209d04fa2243d22b76c2b85c
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-25 14:37:59 +0200

    Initial commit

---
+++ src/main/java/pl/ergohestia/ehj1/ivesta/IVestaApplication.java
@@ -0,0 +1,13 @@
+package pl.ergohestia.ehj1.ivesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IVestaApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(IVestaApplication.class, args);
+    }
+
+}
