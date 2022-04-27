ecee99b09e75f4c908fa4931759185d0626750bf
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-27 14:13:48 +0200

    Add find vehicle ByID;

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
@@ -1,11 +1,15 @@
 package pl.ergohestia.ehj1.ivesta.services;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import pl.ergohestia.ehj1.ivesta.adapters.VehicleAdapter;
+import pl.ergohestia.ehj1.ivesta.entities.Vehicle;
 import pl.ergohestia.ehj1.ivesta.model.VehicleDto;
 import pl.ergohestia.ehj1.ivesta.repository.VehicleRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class VehicleService{
@@ -23,7 +27,11 @@ public class VehicleService{
                 .stream()
                 .map(vehicleAdapter::convertToVehicleDto)
                 .toList();
+    }
 
+    public Vehicle getVehicleById(UUID id) {
+        return vehicleRepository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find vehicle."));
     }
 //todo
 }
