5e1b79d99519c7a27a43af69fa0d79f4b3ed651f
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-04-27 13:54:29 +0200

    Implement findAll

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
@@ -1,10 +1,29 @@
 package pl.ergohestia.ehj1.ivesta.services;
 
-import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.ergohestia.ehj1.ivesta.adapters.VehicleAdapter;
+import pl.ergohestia.ehj1.ivesta.model.VehicleDto;
+import pl.ergohestia.ehj1.ivesta.repository.VehicleRepository;
 
+import java.util.List;
 
-@NoArgsConstructor
-@org.springframework.stereotype.Service
+@Service
 public class VehicleService{
+
+    private final VehicleRepository vehicleRepository;
+    private final VehicleAdapter vehicleAdapter;
+
+    public VehicleService(VehicleRepository vehicleRepository, VehicleAdapter vehicleAdapter) {
+        this.vehicleRepository = vehicleRepository;
+        this.vehicleAdapter = vehicleAdapter;
+    }
+
+    public List<VehicleDto> findAll() {
+        return vehicleRepository.findAll()
+                .stream()
+                .map(vehicleAdapter::convertToVehicleDto)
+                .toList();
+
+    }
 //todo
 }
