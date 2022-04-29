3effb007fc2cb83aca2c26bfc9bab905ee8707af
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-04-29 10:28:18 +0200

    add exception

---
+++ src/main/java/pl/ergohestia/ehj1/ivesta/exception/ResourceNotFound.java
@@ -0,0 +1,8 @@
+package pl.ergohestia.ehj1.ivesta.exception;
+
+public class ResourceNotFound extends RuntimeException {
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
