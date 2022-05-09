6ec094d7bb544c9408d9b117ee921c024e5b0942
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-05-09 21:52:01 +0200

    add Enum

---
+++ src/main/java/pl/ergohestia/ehj1/ivesta/model/Availability.java
@@ -0,0 +1,10 @@
+package pl.ergohestia.ehj1.ivesta.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum Availability {
+    @JsonProperty("active")
+    ACTIVE,
+    @JsonProperty("inactive")
+    INACTIVE
+}
