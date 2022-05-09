56aa646bcfbdead7f968ae6101c3b4e23bb3359e
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-05-09 18:54:16 +0200

    add named queries

--- src/main/java/pl/ergohestia/ehj1/ivesta/repository/RouteRepository.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/repository/RouteRepository.java
@@ -1,5 +1,7 @@
 package pl.ergohestia.ehj1.ivesta.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.ergohestia.ehj1.ivesta.entities.Route;
@@ -8,4 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, UUID> {
+
+    Page<Route> findAllByDriverIsNullAndVehicleIsNull(Pageable pageable);
+    Page<Route> findAllByDriverIsNotNullAndVehicleIsNull(Pageable pageable);
+    Page<Route> findAllByDriverIsNullAndVehicleIsNotNull(Pageable pageable);
 }
