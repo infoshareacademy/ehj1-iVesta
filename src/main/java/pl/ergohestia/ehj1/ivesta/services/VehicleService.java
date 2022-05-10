9648ad596335aa713db79d794befee00ab883e3a
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-05-10 23:44:20 +0200

    add getVehicleByCargoType methode

--- src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/services/VehicleService.java
@@ -7,11 +7,13 @@ import pl.ergohestia.ehj1.ivesta.adapters.VehicleAdapter;
 import pl.ergohestia.ehj1.ivesta.entities.Vehicle;
 import pl.ergohestia.ehj1.ivesta.exception.ResourceNotFound;
 import pl.ergohestia.ehj1.ivesta.model.Availability;
+import pl.ergohestia.ehj1.ivesta.model.TransportType;
 import pl.ergohestia.ehj1.ivesta.model.VehicleDto;
 import pl.ergohestia.ehj1.ivesta.repository.VehicleRepository;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class VehicleService {
@@ -41,37 +43,54 @@ public class VehicleService {
         Vehicle savedVehicle = vehicleRepository.save(vehicle);
         return vehicleAdapter.convertToVehicleDto(savedVehicle);
     }
+
     public void deleteById(UUID id) {
-        if(vehicleRepository.existsById(id)){
+        if (vehicleRepository.existsById(id)) {
             vehicleRepository.deleteById(id);
-        }else throw new ResourceNotFound("Id does not exist in database.");
+        } else throw new ResourceNotFound("Id does not exist in database.");
     }
 
     public VehicleDto updateVehicle(UUID id, VehicleDto vehicleDto) {
         return vehicleRepository.findById(id)
                 .map(vehicle -> {
-                        vehicle.setAvailability(vehicleDto.getAvailability());
-                    if (vehicleDto.getBrand() != null && !vehicleDto.getBrand().isBlank()){
-                    vehicle.setBrand(vehicleDto.getBrand());}
-                    if (vehicleDto.getVehicleCategory() != null && !vehicleDto.getVehicleCategory().isBlank()){
-                    vehicle.setVehicleCategory(vehicleDto.getVehicleCategory());}
-                    if (vehicleDto.getModel() != null && !vehicleDto.getModel().isBlank()){
-                    vehicle.setModel(vehicleDto.getModel());}
-                    if (vehicleDto.getFuelType() != null && !vehicleDto.getFuelType().isBlank()){
-                    vehicle.setFuelType(vehicleDto.getFuelType());}
-                    if (vehicleDto.getNumberOfSeats() != 0){
-                    vehicle.setNumberOfSeats(vehicleDto.getNumberOfSeats());}
-                    if (vehicleDto.getWeightLimit() != 0){
-                    vehicle.setWeightLimit(vehicleDto.getWeightLimit());}
+                    vehicle.setAvailability(vehicleDto.getAvailability());
+                    if (vehicleDto.getBrand() != null && !vehicleDto.getBrand().isBlank()) {
+                        vehicle.setBrand(vehicleDto.getBrand());
+                    }
+                    if (vehicleDto.getVehicleCategory() != null && !vehicleDto.getVehicleCategory().isBlank()) {
+                        vehicle.setVehicleCategory(vehicleDto.getVehicleCategory());
+                    }
+                    if (vehicleDto.getModel() != null && !vehicleDto.getModel().isBlank()) {
+                        vehicle.setModel(vehicleDto.getModel());
+                    }
+                    if (vehicleDto.getFuelType() != null && !vehicleDto.getFuelType().isBlank()) {
+                        vehicle.setFuelType(vehicleDto.getFuelType());
+                    }
+                    if (vehicleDto.getNumberOfSeats() != 0) {
+                        vehicle.setNumberOfSeats(vehicleDto.getNumberOfSeats());
+                    }
+                    if (vehicleDto.getWeightLimit() != 0) {
+                        vehicle.setWeightLimit(vehicleDto.getWeightLimit());
+                    }
                     return vehicleAdapter.convertToVehicleDto(vehicleRepository.save(vehicle));
-          })
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_MODIFIED,"Nothing was changed."));
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_MODIFIED, "Nothing was changed."));
     }
 
     public VehicleDto setVehicleStatus(UUID id, Availability availability) {
         var vehicleDto = getVehicleById(id);
         vehicleDto.setAvailability(availability);
-        updateVehicle(id,vehicleDto);
+        updateVehicle(id, vehicleDto);
         return vehicleDto;
+    }
+
+    public List<VehicleDto> getVehicleByCargoType(String transport) {
+        List<VehicleDto> result = null;
+        if (transport.equalsIgnoreCase("cargo")) {
+            result = vehicleRepository.findAlCargoTransporters().stream().map(vehicleAdapter::convertToVehicleDto).collect(Collectors.toList());
+        } else if (transport.equalsIgnoreCase("passengers")) {
+            result = vehicleRepository.findAllBusses().stream().map(vehicleAdapter::convertToVehicleDto).collect(Collectors.toList());
+        }
+        return result;
     }
 }
