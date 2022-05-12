0042e9dd9077af609a7805156238baaefdb61aa2
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-05-12 11:21:31 +0200

    add license to vehicle

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO drivers(ID, LAST_NAME, LICENSE, NAME)
 VALUES ('9e01475a-c61e-11ec-9d64-0242ac120002', 'Sram', 'A', 'Janusz'),
-       ('9e014a34-c61e-11ec-9d64-0242ac12000', 'Jachaś', 'C+E', 'Domino'),
+       ('9e014a34-c61e-11ec-9d64-0242ac12000', 'Jachaś', 'C_E', 'Domino'),
        ('9e015b82-c61e-11ec-9d64-0242ac12000', 'Bomba', 'B', 'Tytus'),
        ('9e01598e-c61e-11ec-9d64-0242ac12000', 'Koczkodan', 'A', 'Sniezny'),
        ('9e01597e-c61e-11ec-9d64-0242ac12000', 'Bomba', 'B', 'Bogdan'),
@@ -17,17 +17,18 @@ VALUES ('9e01475a-c61e-11ec-9d64-0242ac120002', 'Sram', 'A', 'Janusz'),
        ('19a6a4cc-c61f-11ec-9d64-0242ac120002', 'James', 'B', 'McAvoy'),
        ('1b8e4c90-c61f-11ec-9d64-0242ac120002', 'Anya', 'A', 'Taylor-Joy');
 
-INSERT INTO vehicles(ID, VEHICLE_STATUS, BRAND, MODEL, NUMBER_OF_SEATS, VEHICLE_CATEGORY, WEIGHT_LIMIT)
-VALUES ('56d60854-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9001', 3, 'Dostawczy', 2000),('59096968-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9002', 2, 'Dostawczy', 2000),
-       ('5aae7452-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9003', 2, 'Dostawczy', 2000),
-       ('5c1f1d46-c621-11ec-9d64-0242ac120002','INACTIVE','Man', 'XYZ 9004', 2, 'Dostawczy', 2000),
-       ('604bdfbc-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny M', 8, 'Osobowy', 1500),
-       ('619d1bce-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny L', 8, 'Osobowy', 1500),
-       ('633ab108-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny XL', 8, 'Osobowy', 1500),
-       ('82015ae2-c621-11ec-9d64-0242ac120002','INACTIVE','Volvo', 'Mocarny 100', 3, 'Ciężarowy', 15000),
-       ('84560de2-c621-11ec-9d64-0242ac120002','ACTIVE','Volvo', 'Mocarny 70', 3, 'Ciężarowy', 15000),
-       ('8638c37a-c621-11ec-9d64-0242ac120002','ACTIVE','Volvo', 'Mocarny 60', 3, 'Ciężarowy', 15000),
-       ('87fc1c70-c621-11ec-9d64-0242ac120002','INACTIVE','Volvo', 'Mocarny 50', 3, 'Ciężarowy', 15000);
+INSERT INTO vehicles(ID, VEHICLE_STATUS, BRAND, MODEL, NUMBER_OF_SEATS,LICENSE, VEHICLE_CATEGORY, WEIGHT_LIMIT)
+VALUES ('56d60854-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9001', 3,'C' ,'Dostawczy', 2000),
+       ('59096968-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9002', 2, 'C','Dostawczy', 2000),
+       ('5aae7452-c621-11ec-9d64-0242ac120002','ACTIVE','Man', 'XYZ 9003', 2, 'C' ,'Dostawczy', 2000),
+       ('5c1f1d46-c621-11ec-9d64-0242ac120002','INACTIVE','Man', 'XYZ 9004', 2, 'C' ,'Dostawczy', 2000),
+       ('604bdfbc-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny M', 8, 'C' ,'Osobowy', 1500),
+       ('619d1bce-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny L', 8, 'B' ,'Osobowy', 1500),
+       ('633ab108-c621-11ec-9d64-0242ac120002','ACTIVE','Toyota', 'Super X Extra Mocny XL', 8, 'B' ,'Osobowy', 1500),
+       ('82015ae2-c621-11ec-9d64-0242ac120002','INACTIVE','Volvo', 'Mocarny 100', 3, 'B' ,'Ciężarowy', 15000),
+       ('84560de2-c621-11ec-9d64-0242ac120002','ACTIVE','Volvo', 'Mocarny 70', 3, 'C_E' ,'Ciężarowy', 15000),
+       ('8638c37a-c621-11ec-9d64-0242ac120002','ACTIVE','Volvo', 'Mocarny 60', 3, 'C_E' ,'Ciężarowy', 15000),
+       ('87fc1c70-c621-11ec-9d64-0242ac120002','INACTIVE','Volvo', 'Mocarny 50', 3, 'C_E' ,'Ciężarowy', 15000);
 
 
 
