4bd7b721b6a4daa042d41432c1fe51de40767865
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-05-13 09:31:59 +0200

    tests

--- src/test/java/pl/ergohestia/ehj1/ivesta/services/RouteServiceTest.java
+++ src/test/java/pl/ergohestia/ehj1/ivesta/services/RouteServiceTest.java
@@ -10,29 +10,32 @@ import pl.ergohestia.ehj1.ivesta.entities.Route;
 import pl.ergohestia.ehj1.ivesta.model.RouteDto;
 import pl.ergohestia.ehj1.ivesta.model.TransportType;
 import pl.ergohestia.ehj1.ivesta.repository.RouteRepository;
+import pl.ergohestia.ehj1.ivesta.request.RouteRequest;
 
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RouteServiceTest {
 
+    @MockBean
+    private RouteRepository routeRepository;
+
     @Autowired
     private RouteService sut;
 
     @Autowired
-    private RouteAdapter routeAdapter;
-
-    @MockBean
-    private RouteRepository routeRepository;
+    RouteAdapter routeAdapter;
 
     @Test
     void shouldGetEmptyListWhenNoRoutes() {
         // given
-        Mockito.when(sut.getAllRoutes()).thenReturn(Collections.emptyList());
+        Mockito.when(routeRepository.findAll()).thenReturn(Collections.emptyList());
 
         // when
         List<RouteDto> result = sut.getAllRoutes();
@@ -59,35 +62,63 @@ class RouteServiceTest {
                 TransportType.PASSENGERS,
                 20,
                 LocalDate.parse("2022-05-10"));
-        Mockito.when(sut.getAllRoutes()).thenReturn(List.of(
-                routeAdapter.convertToRouteDto(testRoute1),
-                routeAdapter.convertToRouteDto(testRoute2)));
+        Mockito.when(routeRepository.findAll()).thenReturn(List.of(testRoute1, testRoute2));
 
         // when
         List<RouteDto> result = sut.getAllRoutes();
 
         // then
         assertThat(result).hasSize(2);
+        assertThat(result.get(0)).satisfies(routeDto -> {
+            assertThat(routeDto.getStartAddress()).isEqualTo(routeDto.getStartAddress());
+            assertThat(routeDto.getDate()).isEqualTo(routeDto.getDate());
+        });
 
     }
 
     @Test
     void shouldGetRouteById() {
         // given
+        UUID testId = UUID.fromString("b6f8f2d4-b556-4971-8a5b-f87e22bdb65f");
+        Route testRoute1 = new Route(
+                "testStart",
+                "testDestination",
+                10,
+                TransportType.CARGO,
+                10,
+                LocalDate.parse("2022-05-20"));
+        Mockito.when(routeRepository.findById(testId)).thenReturn(Optional.of(testRoute1));
 
         // when
+        RouteDto result = sut.getRouteById(testId);
 
         // then
+        assertThat(result).satisfies(routeDto -> {
+            assertThat(routeDto.getStartAddress()).isEqualTo(routeDto.getStartAddress());
+            assertThat(routeDto.getTransportType()).isEqualTo(routeDto.getTransportType());
+            assertThat(routeDto.getDate()).isEqualTo(routeDto.getDate());
+        });
 
     }
 
     @Test
     void shouldAddRoute() {
         // given
+        RouteRequest testRouteRequest = new RouteRequest(
+                "testStart",
+                "testDestination",
+                10,
+                TransportType.CARGO,
+                10,
+                LocalDate.parse("2022-05-20"));
+        Mockito.when(routeRepository.save(routeAdapter.convertRouteRequestToRoute(testRouteRequest)))
+                .thenReturn(routeAdapter.convertRouteRequestToRoute(testRouteRequest));
 
         // when
+        RouteDto result = sut.addRoute(testRouteRequest);
 
         // then
+        assertThat(result.getStartAddress()).isEqualTo(testRouteRequest.getStartAddress());
 
     }
 
