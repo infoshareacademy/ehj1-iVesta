05700a5639684179c1c81d1b439174d90315b005
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-05-26 13:01:56 +0200

    add new Exception

--- src/main/java/pl/ergohestia/ehj1/ivesta/exception/UnableToDeleteResource.java
+++ src/main/java/pl/ergohestia/ehj1/ivesta/exception/UnableToDeleteResource.java
@@ -1,5 +1,9 @@
 package pl.ergohestia.ehj1.ivesta.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnableToDeleteResource extends RuntimeException{
     public  UnableToDeleteResource(String message){super(message);}
 }
