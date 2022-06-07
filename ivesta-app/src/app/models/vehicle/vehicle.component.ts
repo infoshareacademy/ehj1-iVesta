60f2c38e52a362d9ded085d02463467062e65618
Krzysztof Dudziak <k.dudziak@yahoo.com>
2022-06-07 15:44:44 +0200

    added vehicle component

---
+++ ivesta-app/src/app/models/vehicle/vehicle.component.ts
@@ -0,0 +1,11 @@
+export interface VehicleComponent  {
+  id: number | undefined;
+  marka: string | undefined;
+  model: string | undefined;
+  "kategoria-pojazdu": string | undefined;
+  license: string | undefined;
+  "liczba-miejsc-ogolem": number | undefined;
+  "rodzaj-paliwa": string | undefined;
+  "max-ladownosc": number | undefined;
+  availability: string | undefined;
+}
