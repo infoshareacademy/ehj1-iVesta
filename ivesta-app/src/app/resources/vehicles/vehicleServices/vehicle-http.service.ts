b86874d0ad1ac13ffacaa4876d281f803adc760b
Krzysztof Dudziak <k.dudziak@yahoo.com>
2022-06-07 15:27:02 +0200

    added vehicle showing function

--- ivesta-app/src/app/resources/vehicles/vehicleServices/vehicle-http.service.ts
+++ ivesta-app/src/app/resources/vehicles/vehicleServices/vehicle-http.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {VehicleComponent} from "../../../models/vehicle/vehicle.component";
+
+const VEHICLES_API_PATH='http://localhost:8080/api/vehicles';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleHttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetch(){
+    return this.http.get<VehicleComponent[]>(VEHICLES_API_PATH);
+  }
 }
--- ivesta-app/src/app/resources/vehicles/vehicles/vehicles.component.ts
+++ ivesta-app/src/app/resources/vehicles/vehicles/vehicles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {VehicleHttpService} from "../vehicleServices/vehicle-http.service";
+import {VehicleComponent} from "../../../models/vehicle/vehicle.component";
 
 @Component({
   selector: 'app-vehicles',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiclesComponent implements OnInit {
 
-  constructor() { }
+  vehicles: VehicleComponent[] = [];
+
+  constructor(private vehicleService: VehicleHttpService) { }
 
   ngOnInit(): void {
+    this.fetchVehicles();
+  }
+  fetchVehicles(){
+    this.vehicleService.fetch().subscribe(response=>{
+      this.vehicles = response;
+    })
+    console.log(this.vehicles);
   }
 
 }
