9b9406f3c56c707f625b01513d1e9ae0bb96783e
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-06-13 20:30:32 +0200

    add proper router to routes

---
+++ ivesta-app/src/app/pages/routes/routes-http.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Route} from "../../model/route";
+
+const ROUTES_API_PATH='http://localhost:8080/api/routes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoutesHttpService {
+
+  constructor(private http: HttpClient) { }
+
+  fetch() {
+    return this.http.get<Route[]>(ROUTES_API_PATH);
+  }
+}
