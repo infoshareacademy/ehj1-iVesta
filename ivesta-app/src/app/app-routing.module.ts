df11e3e2f49f785bccfb490aaa2c45fc4c412d9b
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-06-13 16:00:18 +0200

    add home

--- ivesta-app/src/app/app-routing.module.ts
+++ ivesta-app/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {path: 'drivers', loadChildren: () => import('./pages/drivers/drivers.module').then(m => m.DriversModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
 ];
 
 @NgModule({
