3bfeb4870dc8272186af9c9f803adfe4bf42707e
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-06-15 08:16:28 +0200

    add shadows recolor buttons

--- ivesta-app/src/app/pages/vehicles/edit-vehicle/edit-vehicle.component.ts
+++ ivesta-app/src/app/pages/vehicles/edit-vehicle/edit-vehicle.component.ts
@@ -36,5 +36,5 @@ export class EditVehicleComponent implements OnInit {
     this.form.reset();
   }
 
-  licenses = [ 'AM', 'A1', 'A2', 'A', 'B', 'B1', 'B+E', 'C', 'C1', 'C1+E', 'C+E', 'D', 'D1', 'D1+E', 'D+E', 'T']
+  licenseName = [ 'AM', 'A1', 'A2', 'A', 'B', 'B1', 'B+E', 'C', 'C1', 'C1+E', 'C+E', 'D', 'D1', 'D1+E', 'D+E', 'T']
 }
