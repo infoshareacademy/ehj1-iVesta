380319fa7b793514f8db6e8f8d3d0c49170f0f49
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-06-20 13:43:29 +0200

    unknow path redirect to home

--- ivesta-app/src/app/app-routing.module.ts
+++ ivesta-app/src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import {HomeComponent} from "./pages/home/home.component";
 
 const routes: Routes = [
   {path: 'drivers', loadChildren: () => import('./pages/drivers/drivers.module').then(m => m.DriversModule)},
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'routes', loadChildren: () => import('./pages/routes/routes.module').then(m => m.RoutesModule) },
-  { path: 'vehicles', loadChildren: () => import('./pages/vehicles/vehicles.module').then(m => m.VehiclesModule) },
-  {path:'', component:HomeComponent}
+  {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'routes', loadChildren: () => import('./pages/routes/routes.module').then(m => m.RoutesModule)},
+  {path: 'vehicles', loadChildren: () => import('./pages/vehicles/vehicles.module').then(m => m.VehiclesModule)},
+  {path: '**', redirectTo: 'home'}
+
 ];
 
 @NgModule({
