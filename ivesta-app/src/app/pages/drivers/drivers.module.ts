da3f4c18eece0d475370d8acc84a31d2f1275232
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-06-29 10:51:54 +0200

    simple front page

--- ivesta-app/src/app/pages/drivers/drivers.module.ts
+++ ivesta-app/src/app/pages/drivers/drivers.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DriversRoutingModule } from './drivers-routing.module';
 import { DriversComponent } from './drivers.component';
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 @NgModule({
   declarations: [
     DriversComponent
   ],
-  imports: [
-    CommonModule,
-    DriversRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        DriversRoutingModule,
+        MatPaginatorModule
+    ]
 })
 export class DriversModule { }
